47699ce161e59140c5346d9fcf9b997511c2eb92
Sup_ <1099982811@qq.com>
2017-09-13 19:36:49 +0800

    70-74

---
+++ 73.php
@@ -0,0 +1,6 @@
+<?php
+$data =[
+	['id'=>1,'title'=>'s','price'=> 300,'num'=>3],
+	['id'=>1,'title'=>'u','price'=> 1300,'num'=>3],
+];
+eaco json_encode($data);
---
+++ test/02/73.php
@@ -0,0 +1,6 @@
+<?php
+$data = [
+	['id'=>1,'title'=>'苹果','price'=> 300,'num'=>3],
+	['id'=>1,'title'=>'橘子','price'=> 1300,'num'=>3],
+];
+echo json_encode($data);

b6972231ec6b59e48cb24434989139a22b0adf34
Sup_ <1099982811@qq.com>
2017-10-20 11:17:41 +0800

    book

---
+++ demo03/index.js
@@ -0,0 +1,5 @@
+function sum(a, b) {
+  return a+b;
+}
+var s = sum(3,3);
+console.log('sum'+s)
--- demo03/src/App.vue
+++ demo03/src/App.vue
@@ -1,7 +1,12 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
+    <nav>
+      <ul>
+        <li><a href="">首页</a></li>
+        <li><a href="">关注</a></li>
+        <li><a href="">栏目</a></li>
+      </ul>
+    </nav>
   </div>
 </template>
 
@@ -12,12 +17,5 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
